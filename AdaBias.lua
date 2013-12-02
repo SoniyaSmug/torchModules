8e11b2089b33ddb7149ddc577c080ebcfe3fd350
Mikael <mbh305@nyu.edu>
2013-12-02 14:14:15 -0500

    Fixed running mean/variance

---
+++ AdaBias.lua
@@ -0,0 +1,122 @@
+local AdaBias, parent = torch.class('nn.AdaBias','nn.Module')
+
+function AdaBias:__init(inputSize, centering, lambda, alpha)
+	parent.__init(self)
+
+	self.bias = torch.Tensor(inputSize)
+	if centering == 2 then
+		self.gradBias = torch.Tensor(inputSize)
+	end
+	self.sampleMean = torch.Tensor(inputSize)
+	self.sampleVariance = torch.Tensor(inputSize)
+	self.mean = torch.Tensor(inputSize)
+	self.variance = torch.Tensor(inputSize)
+	self.lambda = torch.Tensor(inputSize):fill(lambda)
+	self.alpha = alpha or 1e-3
+	self.centering = centering
+	self:reset()
+end
+
+function AdaBias:reset()
+	self.bias:zero()
+	self.mean:zero()
+	self.variance:zero()
+end
+
+function AdaBias:updateOutput(input)
+	if input:dim() == 1 then
+		self.output:resize(self.bias:size(1))
+		self.output:copy(input)
+      -- update running average 
+      self.mean:mul(1-self.alpha)
+      self.mean:add(self.alpha,input)
+      -- update running variance
+      self.variance:mul(1-self.alpha)
+      self.variance:add(self.alpha, torch.pow(input-self.mean,2))
+		-- if using hard centering, compute bias which sets (1-lambda) of samples to 0
+      if self.centering == 1 then
+         self.bias = inormcdf(self.lambda, self.mean, torch.sqrt(self.variance))
+      end
+      -- add the bias
+      self.output:add(self.bias)
+
+	elseif input:dim() == 2 then
+		local nframe = input:size(1)
+		local nunit = self.bias:size(1)
+		self.output:resize(nframe,nunit)
+		self.output:copy(input)
+      -- update running average
+		self.sampleMean = torch.mean(input,1)
+      self.mean:mul(1-self.alpha)
+      self.mean:add(self.alpha, self.sampleMean)
+		-- update running variance
+      self.variance:mul(1-self.alpha)
+		self.mean:resize(1,nunit)
+		self.sampleVariance=torch.mean(torch.pow(input - torch.expand(self.mean,nframe,nunit),2),1)
+		self.variance:add(self.alpha,self.sampleVariance)
+		self.mean:resize(nunit)
+		if self.centering == 1 then
+			self.bias = inormcdf(self.lambda,self.mean, torch.sqrt(self.variance))
+		end
+		self.output:addr(1, input.new(nframe):fill(1), self.bias)
+	else 
+		error('input must be vector or matrix')
+	end
+	return self.output
+end
+
+
+function AdaBias:updateGradInput(input, gradOutput)
+	if self.gradInput then 
+		self.gradInput:resizeAs(input)
+		self.gradInput:copy(gradOutput)
+		-- TODO: account for dependence between running mean/variance and current sample (should be small)
+		--[[ 
+		local d1=torch.Tensor(self.mean:size()):fill(1+self.alpha)
+		local d2=(self.sampleMean-self.mean)*2*self.alpha*inormcdf(torch.Tensor({self.lambda[1]}),0,1)[1]
+		local s=inormcdf(torch.Tensor({self.lambda[1]}),0,1)[1]
+		self.gradInput:cmul(torch:Tensor(input:size()):fill(1+self.alpha) + (self.sampleMean-self.mean)*2*self.alpha*inormcdf(torch.Tensor({self.lambda[1]}),0,1)[1])
+		--]]
+		return self.gradInput
+	end
+end
+
+
+function AdaBias:accGradParameters(input, gradOutput, scale)
+	scale = scale or 1
+	if input:dim() == 1 then
+		if self.centering == 2 then
+			self.gradBias:add(scale, gradOutput)
+			self.gradBias:add(scale*self.lambda[1]*2, self.mean - self.bias)
+		end
+	elseif input:dim() == 2 then
+		local nframe = input:size(1)
+		local nunit = self.bias:size(1)
+		if self.centering == 2 then
+			self.gradBias:addmv(scale, gradOutput:t(), input.new(nframe):fill(1))
+			local diff = self.mean - self.bias
+			diff:div(diff:norm())
+         self.gradBias:add(scale*self.lambda[1]*2, diff)
+		end
+	end
+end
+
+-- If we're doing hard centering, specify that the bias is not trainable
+function AdaBias:parameters()
+	if self.centering == 1 then
+		return 
+	elseif self.centering == 2 then
+		return {self.bias}, {self.gradBias}
+	end
+end
+
+
+
+
+
+
+
+
+
+
+	
---
+++ testBias.lua
@@ -0,0 +1,109 @@
+require 'nn'
+require 'optim'
+
+dofile('AdaBias.lua')
+dofile('stats.lua')
+dofile('loadData.lua')
+torch.manualSeed(123)
+torch.setdefaulttensortype('torch.FloatTensor')
+
+-- model params
+nHidden=5
+lambda=0.8
+batchSize=10
+nClasses=10
+-- choose 1 for hard centering
+centering=1
+
+-- load MNIST
+nSamples=1000
+data,targets=loadData('MNIST','train',nSamples)
+
+-- make model
+nInputs=data:size(2)
+nHidden=5
+model=nn.Sequential()
+model:add(nn.Linear(nInputs,nHidden))
+model:add(nn.AdaBias(nHidden,centering,lambda,alpha))
+model:add(nn.Threshold())
+model:add(nn.Linear(nHidden,nClasses))
+model:add(nn.LogSoftMax())
+criterion=nn.ClassNLLCriterion()
+
+-- define gradient evaluation 
+w,dL_dw=model:getParameters()
+
+feval = function(w_new)
+	_nidx_ = (_nidx_ or 0) + 1
+	if _nidx_ > nSamples-batchSize then _nidx_ = 1 end
+	local inputs=data[{{_nidx_,_nidx_+batchSize-1},{}}]
+	--local inputs=data[_nidx_]
+	local target=targets[_nidx_]
+	local target=targets[{{_nidx_,_nidx_+batchSize-1},{}}]
+	dL_dw:zero()
+	local loss_w = criterion:forward(model:forward(inputs),target)
+	model:backward(inputs,criterion:backward(model.output,target))
+	return loss_w,dL_dw
+end
+
+-- optimization parameters
+sgd_params = {
+   learningRate = 1e-3,
+   learningRateDecay = 1e-4,
+   weightDecay = 0,
+   momentum = 0
+}
+epochs=20
+
+-- train!
+for i=1,epochs do
+	current_loss=0
+	for j=1,nSamples do
+		_,fs=optim.sgd(feval,w,sgdParams)
+		current_loss = current_loss + fs[1]
+	end
+	current_loss=current_loss/nSamples
+	print('epoch=' .. i .. ', loss=' .. current_loss .. '\n')
+end
+
+-- plot some distributions
+m1=nn.Sequential():add(model.modules[1]):add(model.modules[2])
+d1=m1:forward(data)
+gnuplot.hist(d1[{{},1}])
+
+-- see what proportion of samples is set to 0
+m2=nn.Sequential():add(m1):add(model.modules[3])
+d2=m2:forward(data)
+a=torch.Tensor(nHidden)
+for i=1,nHidden do
+	a[i]=torch.sum(torch.gt(d2[{{},i}],0))/d2:size(1)
+end
+print('Proportion of samples set to 0:')
+print(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--exit()
