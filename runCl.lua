bf10d31316f8025ceabd797ed4f4406fa690a636
Mikael <mbh305@nyu.edu>
2013-12-12 17:59:00 -0500

    Added utils for running experiments

--- runCl.lua
+++ runCl.lua
@@ -11,11 +11,15 @@ cmd:option('-dataset', 'MNIST', 'dataset to train on: MNIST | CIFAR-10 | SVHN')
 cmd:option('-model','linear', 'type of model to construct: linear | mlp | convnet')
 cmd:option('-batchSize', 1, 'mini-batch size (1=pure stochastic)')
 cmd:option('-epochs', 5, 'number of passes over the dataset')
-cmd:option('-plot',false,'plot intermediate results')
+cmd:option('-plot',0,'plot intermediate results')
 cmd:option('-type','double','type: double | float | cuda')
+cmd:option('-save','Results/','directory to save results in')
+cmd:option('-lambda',0.5, 'sparsity (if applicable)')
+cmd:option('-nhidden',20,'number of hidden units for MLP')
 
 -- parse options
 opt = cmd:parse(arg or {})
+opt.save = opt.save .. '/' .. opt.dataset .. '/' .. opt.model
 print(opt)
 
 --------------------------------------------------------------------------------------
@@ -32,12 +36,11 @@ elseif opt.dataset == 'CIFAR-10' then
 	nfeats = trdata:size(2)
 	height = trdata:size(3)
 	width = trdata:size(4)
-	nInputs = nFeats*width*height
+	nInputs = nfeats*width*height
 end
 
 classes = {'1','2','3','4','5','6','7','8','9','10'}
 nOutputs = 10
-nInputs = trdata:size(2)
 
 --------------------------------------------------------------------------------------
 -- MODEL
@@ -56,8 +59,20 @@ elseif opt.model == 'mlp' then
 	model:add(nn.Tanh())
 	model:add(nn.Linear(nHidden,nOutputs))
 
-elseif opt.model == 'convnet' then
+elseif opt.model == 'mlp-adabias' then
+	dofile('AdaBias2.lua')
+	nHidden = opt.nhidden
+	model = nn.Sequential()
+	model:add(nn.Reshape(nInputs))
+	model:add(nn.Linear(nInputs,nHidden))
+	model:add(nn.AdaBias(nHidden,1,opt.lambda))
+	model:add(nn.Threshold())
+	model:add(nn.Linear(nHidden,nHidden))
+	model:add(nn.AdaBias(nHidden,1,opt.lambda))
+	model:add(nn.Threshold())
+	model:add(nn.Linear(nHidden,nOutputs))
 
+elseif opt.model == 'convnet' then
 	-- number of hidden units at different layers
 	nstates = {64,64,128}
 	filtsize = 5
@@ -67,7 +82,7 @@ elseif opt.model == 'convnet' then
 
    -- stage 1 : filter bank -> squashing -> L2 pooling -> normalization
    model:add(nn.SpatialConvolutionMM(nfeats, nstates[1], filtsize, filtsize))
-   model:add(nn.Tanh())
+	model:add(nn.Tanh())
    model:add(nn.SpatialLPPooling(nstates[1],2,poolsize,poolsize,poolsize,poolsize))
    model:add(nn.SpatialSubtractiveNormalization(nstates[1], normkernel))
 
@@ -83,6 +98,31 @@ elseif opt.model == 'convnet' then
    model:add(nn.Tanh())
    model:add(nn.Linear(nstates[3], nOutputs))
 
+elseif opt.model == 'convnet-relu' then
+   -- number of hidden units at different layers
+   nstates = {64,64,128}
+   filtsize = 5
+   poolsize = 2
+   normkernel = image.gaussian1D(7)
+   model = nn.Sequential()
+
+   -- stage 1 : filter bank -> squashing -> L2 pooling -> normalization
+   model:add(nn.SpatialConvolutionMM(nfeats, nstates[1], filtsize, filtsize))
+   model:add(nn.Threshold())
+   model:add(nn.SpatialLPPooling(nstates[1],2,poolsize,poolsize,poolsize,poolsize))
+   model:add(nn.SpatialSubtractiveNormalization(nstates[1], normkernel))
+
+   -- stage 2 : filter bank -> squashing -> L2 pooling -> normalization
+   model:add(nn.SpatialConvolutionMM(nstates[1], nstates[2], filtsize, filtsize))
+   model:add(nn.Threshold())
+   model:add(nn.SpatialLPPooling(nstates[2],2,poolsize,poolsize,poolsize,poolsize))
+   model:add(nn.SpatialSubtractiveNormalization(nstates[2], normkernel))
+
+   -- stage 3 : standard 2-layer neural network
+   model:add(nn.Reshape(nstates[2]*filtsize*filtsize))
+   model:add(nn.Linear(nstates[2]*filtsize*filtsize, nstates[3]))
+   model:add(nn.Threshold())
+   model:add(nn.Linear(nstates[3], nOutputs))
 else 
 	error('unrecognized model type')
 end		
@@ -114,13 +154,22 @@ opt.classes = classes
 ------------------------------------------------------------------------------------------
 
 -- create logger
-trainLogger = optim.Logger('train.log')
-testLogger = optim.Logger('test.log')
+trainLogger = optim.Logger(opt.save .. '/train.log')
+testLogger = optim.Logger(opt.save .. '/test.log')
 
 
 for i = 1,opt.epochs do
+
+	-- train
 	trainModel(model, criterion, trdata, trlabels, opt, trainLogger)
 	testModel(model, tedata, telabels, opt, testLogger)
+
+   --save/log current net
+   local filename = paths.concat(opt.save, 'model_epoch_' .. i  .. '.net')
+   os.execute('mkdir -p ' .. sys.dirname(filename))
+   print('==> saving model to '..filename)
+   torch.save(filename, {model=model,trainLogger=trainLogger, testLogger=testLogger})
+
 end
 
 
--- trainTest.lua
+++ trainTest.lua
@@ -4,13 +4,11 @@ function trainModel(model, criterion, data, labels, opt, logger)
 	local optimMethod = opt.optimMethod
 	local optimState = opt.optimState
 	local batchSize = opt.batchSize
-	local classes = opt.classes
 	local trsize = data:size(1)
-	local confusion = optim.ConfusionMatrix(classes)
-	local verbose = false
+	local confusion = optim.ConfusionMatrix(opt.classes)
 
 	-- get model parameters
-	local w,dL_dw = model:getParameters()
+	w,dL_dw = model:getParameters()
 	-- shuffle at each epoch
 	local shuffle = torch.randperm(trsize)
 	
@@ -20,7 +18,7 @@ function trainModel(model, criterion, data, labels, opt, logger)
 		-- create minibatch
 		local inputs = {}
 		local targets = {}
-		for i = 1,math.min(t+batchSize-1, trsize) do
+		for i = t,math.min(t+batchSize-1, trsize) do
 			-- load new sample
 			local input = data[shuffle[i]]
 			local target = labels[shuffle[i]]
@@ -44,7 +42,7 @@ function trainModel(model, criterion, data, labels, opt, logger)
 			dL_dw:zero()
 			
 			-- L is the average loss
-			L = 0
+			local L = 0
 			
 			-- evaluate function for complete minibatch
 			for i=1,#inputs do
@@ -73,26 +71,16 @@ function trainModel(model, criterion, data, labels, opt, logger)
 		optimMethod(feval,w,optimState)
 	end
 	print('Loss: ' .. L)
-	if verbose then
-		print(confusion)
-	end
+	print(confusion)
 
 	-- record log info
-	if logger then
+	if true then
 		logger:add{['% mean class accuracy (train set)'] = confusion.totalValid * 100}
-		if verbose then
+		if opt.plot then
 			logger:style{['% mean class accuracy (train set)'] = '-'}
 			logger:plot()
 		end
 	end
-
-   --save/log current net
-   local filename = paths.concat(opt.save, 'model.net')
-   os.execute('mkdir -p ' .. sys.dirname(filename))
-   print('==> saving model to '..filename)
-   torch.save(filename, model)
-
-	
 end
 
 
@@ -114,7 +102,7 @@ function testModel(model, data, labels, opt, logger)
 		local pred = model:forward(input)
 		confusion:add(pred,target)
 	end
-	
+
 	print(confusion)
 	
   -- update log/plot
